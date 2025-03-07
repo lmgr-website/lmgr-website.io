db603ff27bf20622f3b5e72664c30fa6951090a5
Anmol G <me@aghadia.com>
2025-03-06 18:34:06 -0800

    added initial nav design

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // Get the div
-const scrollDiv = document.querySelector('.scroll-div');
+const scrollDiv = document.querySelector('.nav');
 
 // Listen for the scroll event on the window
 window.addEventListener('scroll', function() {
     // Check if the page has been scrolled down by a certain amount
     if (window.scrollY > 50) { // Change 50 to whatever scroll position you want
-        scrollDiv.classList.add('scrolled'); // Add the class when scrolled
+        scrollDiv.classList.add('nav-scrolled'); // Add the class when scrolled
     } else {
-        scrollDiv.classList.remove('scrolled'); // Remove the class when back at the top
+        scrollDiv.classList.remove('nav-scrolled'); // Remove the class when back at the top
     }
 });
 
