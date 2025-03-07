eff255d9f0abd431977a98bc764ccb13d65f5a56
Anmol G <me@aghadia.com>
2025-03-06 17:32:21 -0800

    added initial files

---
+++ script.js
@@ -0,0 +1,27 @@
+// Get the div
+const scrollDiv = document.querySelector('.scroll-div');
+
+// Listen for the scroll event on the window
+window.addEventListener('scroll', function() {
+    // Check if the page has been scrolled down by a certain amount
+    if (window.scrollY > 50) { // Change 50 to whatever scroll position you want
+        scrollDiv.classList.add('scrolled'); // Add the class when scrolled
+    } else {
+        scrollDiv.classList.remove('scrolled'); // Remove the class when back at the top
+    }
+});
+
+
+
+// Get the div
+heroImage = document.querySelector('.hero-image');
+
+// Listen for the scroll event on the window
+window.addEventListener('scroll', function() {
+    // Check if the page has been scrolled down by a certain amount
+    if (window.scrollY > 50) { // Change 50 to whatever scroll position you want
+        heroImage.classList.add('hero-image-scrolled'); // Add the class when scrolled
+    } else {
+        heroImage.classList.remove('hero-image-scrolled'); // Remove the class when back at the top
+    }
+});
